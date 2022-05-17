2b1edf03ed5ab009fe8a4e4998065c1a74ad687e
David Smidt <dsmidt8@gmail.com>
2022-05-17 18:08:02 -0500

    remove console log

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,19 +2,14 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-
-
 //event listner to add new task
 var createTaskHandler = function() {
-
     event.preventDefault();
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-
-    console.log(event);
 }
 
 formEl.addEventListener("submit", createTaskHandler);
